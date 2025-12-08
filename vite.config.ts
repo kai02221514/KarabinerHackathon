d4d311d588e308f1948951ad531a078cdccdd0c7
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2025-12-08 12:16:59 +0900

    add: supabaseの導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-
-  import { defineConfig } from 'vite';
-  import react from '@vitejs/plugin-react-swc';
-  import path from 'path';
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import env from "vite-plugin-env-compatible";
+  
   export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env" })],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
